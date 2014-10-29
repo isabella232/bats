03a3be7b0e5a8e99afd17fe4bcfff5e1fbe19d01
lindamood <wesley.lindamood@gmail.com>
2014-10-29 11:37:01 -0400

    laying out the looping video

--- www/js/app.js
+++ www/js/app.js
@@ -1,91 +1,32 @@
-// Global jQuery references
-var $shareModal = null;
-var $commentCount = null;
+$(document).ready(function() {
 
-// Global state
-var firstShareLoad = true;
+    $('body').css('opacity', 1);
+    //
+    
 
-/*
- * Run on page load.
- */
-var onDocumentLoad = function(e) {
-    // Cache jQuery references
-    $shareModal = $('#share-modal');
-    $commentCount = $('.comment-count');
-
-    // Bind events
-    $shareModal.on('shown.bs.modal', onShareModalShown);
-    $shareModal.on('hidden.bs.modal', onShareModalHidden);
-
-    // configure ZeroClipboard on share panel
-    ZeroClipboard.config({ swfPath: 'js/lib/ZeroClipboard.swf' });
-    var clippy = new ZeroClipboard($(".clippy"));
-
-    clippy.on('ready', function(readyEvent) {
-        clippy.on('aftercopy', onClippyCopy);
+    var mySound = new buzz.sound("http://stage-apps.npr.org/bats/assets/bats-soundscape.mp3", {
+    preload: true,
+    autoplay: false,
+    loop: true
     });
+    
 
-    renderExampleTemplate();
-    getCommentCount(showCommentCount);
-}
 
-/*
- * Basic templating example.
- */
-var renderExampleTemplate = function() {
-    var context = $.extend(APP_CONFIG, {
-        'template_path': 'jst/example.html',
-        'config': JSON.stringify(APP_CONFIG, null, 4),
-        'copy': JSON.stringify(COPY, null, 4)
-    });
+	$('.audio-controls').on('click', function(){
+		mySound.togglePlay();
+		$(this).toggleClass( "audio-paused" );
+	});
 
-    var html = JST.example(context);
-
-    $('#template-example').html(html);
-}
-
-/*
- * Display the comment count.
- */
-var showCommentCount = function(count) {
-    $commentCount.text(count);
-
-    if (count > 0) {
-        $commentCount.addClass('has-comments');
-    }
-
-    if (count > 1) {
-        $commentCount.next('.comment-label').text('Comments');
-    }
-}
-
-/*
- * Share modal opened.
- */
-var onShareModalShown = function(e) {
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'open-share-discuss']);
-
-    if (firstShareLoad) {
-        loadComments();
-
-        firstShareLoad = false;
-    }
-}
-
-/*
- * Share modal closed.
- */
-var onShareModalHidden = function(e) {
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'close-share-discuss']);
-}
-
-/*
- * Text copied to clipboard.
- */
-var onClippyCopy = function(e) {
-    alert('Copied to your clipboard!');
-
-    _gaq.push(['_trackEvent', APP_CONFIG.PROJECT_SLUG, 'summary-copied']);
-}
-
-$(onDocumentLoad);
+    //start screen
+    
+    $('.btn-go').on('click', function(){
+		mySound.play();
+		$('.start-screen').addClass( "enter-soundscape" );
+		
+		$('.soundscape').css('opacity', 1);
+		$('.titlecard').css('opacity', 0);
+		
+	});
+    
+    
+});
